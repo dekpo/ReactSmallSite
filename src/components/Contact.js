52531d0bd29891d7e1ecb7d353a1b7f7f741ef74
Dekpo <dekpo@me.com>
2021-11-04 15:24:42 +0100

    Prepared Gallery page to fetch API url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,10 @@ const Contact = () => {
         <div className="container-fluid">
           <div className="row"><h1>Contact</h1></div>
           <div className="row">
-            <div className="col-12 col-md-6">
-              <img className="img-fluid" src="https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_1280.jpg" alt="Cabane" />
+            <div className="col-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5648408.552307045!2d-1.6075042511671231!3d46.269866038236074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af4ae98246ab9%3A0x373e98de47360574!2sM2i%20Formation!5e0!3m2!1sfr!2sfr!4v1636035139478!5m2!1sfr!2sfr" width="100%" height="450" loading="lazy"></iframe>
             </div>
-            <div className="col-12 col-md-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            <div className="col-12">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
           </div>
         </div>
       </section> 
