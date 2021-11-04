2eca416ba2d4620b4626b77a0c5e3aa74083391b
Dekpo <dekpo@me.com>
2021-11-04 16:34:31 +0100

    Added API link to fetch and generated a pictures list

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,26 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Gallery = () => {
+  const [galleryData, setGalleryData] = useState([]);
 
- useEffect( ()=>{ alert("yo");}, []);
+  const loadAPI = () => {
+    fetch("https://html-learning.com/dwwm2i/dekpo/public/pictures/list?page=1&limit=6")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setGalleryData(data);
+      })
+  }
+  useEffect(() => { loadAPI() }, []);
 
-    return(
-        <section>
-        <div className="container-fluid">
-          <div className="row"><h1>Gallery</h1></div>
-          <div className="row">
-            <div className="col-12 col-md-4 mb-2">
-              <img className="img-fluid" src="https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_1280.jpg" alt="Cabane" />
-            </div>
-            <div className="col-12 col-md-4 mb-2">
-              <img className="img-fluid" src="https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_1280.jpg" alt="Cabane" />
-            </div>
-            <div className="col-12 col-md-4 mb-2">
-              <img className="img-fluid" src="https://cdn.pixabay.com/photo/2021/10/28/20/20/hut-6750482_1280.jpg" alt="Cabane" />
-            </div>
-          </div>
-        </div>
-      </section> 
+  const ListImages = () => {
+    return (
+      <div className="row">
+        {
+          galleryData.map(item => {
+            return (
+              <div key={ item.id } className="col-12 col-md-4 mb-2 text-center">
+                <img className="img-fluid" src={ item.url } alt="Cabane" />
+                <h2 className="h5">{ item.title }</h2>
+              </div>)
+          })
+        }
+      </div>
     )
+  }
+
+  return (
+    <section>
+      <div className="container-fluid">
+        <div className="row"><h1>Gallery</h1></div>
+        <ListImages />
+      </div>
+    </section>
+  )
 }
 export default Gallery;
